cd8d751af23e451fb5f31750f351dfe33d420376
Digvijay Sethi <59555227+justxdv@users.noreply.github.com>
2023-04-30 15:35:49 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
+import KeyValueStore from "./components/App";
 
 
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<KeyValueStore />, document.getElementById("root"));
